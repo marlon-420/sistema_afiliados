53b902b79932f74b7d78f47f039af3c3eab218d7
Marlon <isaacllerena202@gmail.com>
2024-12-04 16:40:17 -0500

    Ajusta ruta del certificado SSL para Render

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.getenv('DATABASE_HOST')
-    MYSQL_USER = os.getenv('DATABASE_USERNAME')
-    MYSQL_PASSWORD = os.getenv('DATABASE_PASSWORD')
-    MYSQL_DB = os.getenv('DATABASE')
-    MYSQL_SSL_CA = os.getenv('MYSQL_SSL_CA')
+    MYSQL_HOST = os.getenv("DATABASE_HOST")
+    MYSQL_USER = os.getenv("DATABASE_USERNAME")
+    MYSQL_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    MYSQL_DB = os.getenv("DATABASE")
+    MYSQL_SSL_CA = "/etc/ssl/certs/ca-certificates.crt"  # Ruta estándar en Render
